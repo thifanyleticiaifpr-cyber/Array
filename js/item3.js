5448e81a1663021dc47f2a3468cdd9a565e3f87e
thifanyleticiaifpr-cyber <thifany.leticia.ifpr@gmail.com>
2025-09-30 20:33:55 -0300

    ultima (oficial)
    agora a lista aparece

--- js/item3.js
+++ js/item3.js
@@ -3,15 +3,15 @@ var f
 
 do{
    f = prompt("Digite a fruta: ")
-   frutas.push(f)
- 
 
-}while(f!=null)
-
-     if(f=" ")
-    {
-      alert("Clique em cancelar para ver a sua lista")
+   if(f!==null){
+    frutas.push(f)
+   }
+    else if(f===" "){
+        alert("Clique em cancelar para ver sua lista")
     }
+   
+}while(f!=null)
     alert(frutas)
 for(var i=0; i<frutas.length-1;i++){ /*Complemento -1 significa que não vai mostrar o ultimo da lista  */
    document.write("<h2>"+frutas[i]+"</h2>")
